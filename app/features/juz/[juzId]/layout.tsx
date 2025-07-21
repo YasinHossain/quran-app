80efb3943279397eb936f13dbb21936144eecd69
Yasin Hossain <67205970+YasinHossain@users.noreply.github.com>
2025-07-21 21:17:34 +0600

    remove duplicate settings provider from nested layouts

--- app/features/juz/[juzId]/layout.tsx
+++ app/features/juz/[juzId]/layout.tsx
@@ -2,22 +2,19 @@
 import Header from '@/app/components/common/Header';
 import IconSidebar from '@/app/components/common/IconSidebar';
 import SurahListSidebar from '@/app/components/common/SurahListSidebar';
-import { SettingsProvider } from '@/app/context/SettingsContext';
 import { AudioProvider } from '@/app/context/AudioContext';
 
 export default function JuzLayout({ children }: { children: React.ReactNode }) {
   return (
-    <SettingsProvider>
-      <AudioProvider>
-        <div className="h-screen flex flex-col">
-          <Header />
-          <div className="flex flex-grow overflow-hidden">
-            <IconSidebar />
-            <SurahListSidebar />
-            {children}
-          </div>
+    <AudioProvider>
+      <div className="h-screen flex flex-col">
+        <Header />
+        <div className="flex flex-grow overflow-hidden">
+          <IconSidebar />
+          <SurahListSidebar />
+          {children}
         </div>
-      </AudioProvider>
-    </SettingsProvider>
+      </div>
+    </AudioProvider>
   );
 }
