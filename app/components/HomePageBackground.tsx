28dc1b0c1cc5816871184eef5a0d10d43da32190
Yasin Hossain <67205970+YasinHossain@users.noreply.github.com>
2025-07-23 14:21:39 +0600

    Add third background blob

--- app/components/HomePageBackground.tsx
+++ app/components/HomePageBackground.tsx
@@ -4,6 +4,7 @@ export default function HomePageBackground() {
       {/* Decorative Blobs */}
       <div className="absolute top-[-10rem] right-[-10rem] h-72 w-72 rounded-full bg-emerald-400/10 filter blur-3xl opacity-50 dark:bg-emerald-500/10"></div>
       <div className="absolute bottom-[-5rem] left-[-10rem] h-80 w-80 rounded-full bg-cyan-400/10 filter blur-3xl opacity-40 dark:bg-cyan-500/10"></div>
+      <div className="absolute bottom-[20rem] right-[-15rem] w-96 h-96 rounded-full bg-sky-400/10 filter blur-3xl opacity-30 dark:bg-sky-500/10"></div>
     </div>
   );
 }
