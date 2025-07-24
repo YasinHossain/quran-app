31cf2c4d1b65843fa1639e0cd1a598f9029dd749
Yasin Hossain <67205970+YasinHossain@users.noreply.github.com>
2025-07-24 13:01:33 +0600

    Add word display settings

--- types/settings.ts
+++ types/settings.ts
@@ -3,4 +3,7 @@ export interface Settings {
   arabicFontSize: number;
   translationFontSize: number;
   arabicFontFace: string;
+  showByWords: boolean;
+  tajweed: boolean;
+  wordLang: string;
 }
