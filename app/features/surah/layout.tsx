a8f96b289ecea1ac344f4107cd792c86c75b4ec0
Yasin Hossain <67205970+YasinHossain@users.noreply.github.com>
2025-08-04 20:10:51 +0600

    Add shared features layout

--- app/features/surah/layout.tsx
+++ app/features/surah/layout.tsx
@@ -1,25 +1,7 @@
 'use client';
 
-import Header from '@/app/components/common/Header';
-import IconSidebar from '@/app/components/common/IconSidebar';
-import SurahListSidebar from '@/app/components/common/SurahListSidebar';
 import { AudioProvider } from '@/app/context/AudioContext';
 
 export default function SurahLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <AudioProvider>
-      <Header />
-      <div className="h-screen flex flex-col">
-        <div className="flex flex-grow overflow-hidden">
-          <nav aria-label="Primary navigation" className="flex-shrink-0 pt-16">
-            <IconSidebar />
-          </nav>
-          <nav aria-label="Surah navigation" className="flex-shrink-0 pt-16">
-            <SurahListSidebar />
-          </nav>
-          {children}
-        </div>
-      </div>
-    </AudioProvider>
-  );
+  return <AudioProvider>{children}</AudioProvider>;
 }
