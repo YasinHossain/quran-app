6b188fbaea11ee97b54e8de284387d6a1b38b391
Yasin Hossain <67205970+YasinHossain@users.noreply.github.com>
2025-08-13 16:06:09 +0600

    Move Juz interface to dedicated file

--- types/index.ts
+++ types/index.ts
@@ -5,14 +5,6 @@ export * from './tafsir';
 export * from './settings';
 export * from './surah';
 export * from './word';
+export * from './juz';
 
 export type { TafsirResource } from '../lib/api';
-
-export interface Juz {
-  id: number;
-  juz_number: number;
-  verse_mapping: Record<string, string>;
-  first_verse_id: number;
-  last_verse_id: number;
-  verses_count: number;
-}
