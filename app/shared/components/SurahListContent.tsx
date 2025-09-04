0bacd84b98ec08d0e5eb344f70970af31e86d5c0
YasinHossain <yasinhossainyh0@gmail.com>
2025-09-04 21:27:23 +0600

    fixed default export

--- app/shared/components/SurahListContent.tsx
+++ app/shared/components/SurahListContent.tsx
@@ -5,13 +5,13 @@ import { useTranslation } from 'react-i18next';
 import { useParams, usePathname } from 'next/navigation';
 import { Chapter } from '@/types';
 import juzData from '@/data/juz.json';
-import useSidebarScroll from '../surah-sidebar/useSidebarScroll';
-import Surah from '../surah-sidebar/Surah';
-import Juz from '../surah-sidebar/Juz';
-import Page from '../surah-sidebar/Page';
-import SidebarTabs from '../surah-sidebar/components/SidebarTabs';
+import { useSidebarScroll } from '../surah-sidebar/useSidebarScroll';
+import { Surah } from '../surah-sidebar/Surah';
+import { Juz } from '../surah-sidebar/Juz';
+import { Page } from '../surah-sidebar/Page';
+import { SidebarTabs } from '../surah-sidebar/components/SidebarTabs';
 import { SearchInput } from './SearchInput';
-import useSelectionSync from '../surah-sidebar/hooks/useSelectionSync';
+import { useSelectionSync } from '../surah-sidebar/hooks/useSelectionSync';
 
 interface JuzSummary {
   number: number;
